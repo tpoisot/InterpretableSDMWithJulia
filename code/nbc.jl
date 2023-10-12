9e11476edfabdb15f62ce1b90c66be02d4179322
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-12 14:35:34 -0400

    remove z-score (not needed)

--- code/nbc.jl
+++ code/nbc.jl
@@ -1,14 +1,11 @@
 function naivebayes(y::Vector{Bool}, X::Matrix{T}; presence=0.5) where {T <: Number}
-    μ = mapslices(mean, X, dims=1)
-    σ = mapslices(std, X, dims=1)
-    Xpos = (X[findall(y),:] .- μ) ./ σ
-    Xneg = (X[findall(.!y),:] .- μ) ./ σ
+    Xpos = X[findall(y),:]
+    Xneg = X[findall(.!y),:]
     pred_pos = mapslices(x -> Normal(mean(x), std(x)), Xpos, dims=1)
     pred_neg = mapslices(x -> Normal(mean(x), std(x)), Xneg, dims=1)
     function inner_predictor(v::Vector{TN}) where { TN <: Number }
-        nv = (v' .- μ) ./ σ
-        is_pos = prod(pdf.(pred_pos, nv))
-        is_neg = prod(pdf.(pred_neg, nv))
+        is_pos = prod(pdf.(pred_pos, v))
+        is_neg = prod(pdf.(pred_neg, v))
         evid = presence * is_pos + (1 - presence) * is_neg
         return (presence * is_pos)/evid
     end
