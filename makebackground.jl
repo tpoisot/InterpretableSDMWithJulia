05fd2f4b412e6245f9b41bd8333378ce21de41e8
tpoisot <timothee.poisot@umontreal.ca>
2024-09-28 18:50:32 -0400

    fix colors

--- makebackground.jl
+++ makebackground.jl
@@ -5,16 +5,18 @@ provider = RasterData(CHELSA2, BioClim)
 layer =
     0.1SDMLayer(
         provider;
-        layer = "BIO19",
-        left = 5.0,
-        right = 13.0,
-        bottom = 47.0,
-        top = 52.0,
+        layer = "BIO3",
+        left = 10.0,
+        right = 18.0,
+        bottom = 49.0,
+        top = 54.0,
     )
+
+bg_pal = :starrynight
 
 f = Figure(; size = (1600, 1000), figure_padding = 0)
 ax = Axis(f[1, 1])
-heatmap!(ax, quantize(layer, 10); colormap = :pastel)
+heatmap!(ax, quantize(layer, 100); colormap = bg_pal, alpha=0.6)
 hidespines!(ax)
 hidedecorations!(ax)
 current_figure()
